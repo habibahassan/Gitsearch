0767febe2b6e2dafa3f72fc4cfc39c00f02ade33
habibahassan <halimaadan92@gmail.com>
2020-01-18 19:19:36 +0300

    added class git-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitsearchComponent } from './gitsearch/gitsearch.component';
+import { GitFormComponent } from './git-form/git-form.component';
+import { GitHomeComponent } from './git-home/git-home.component';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HoverDirective } from './hover.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitsearchComponent,
+    GitFormComponent,
+    GitHomeComponent,    
+    NavbarComponent,
+    NotFoundComponent,
+    HoverDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
